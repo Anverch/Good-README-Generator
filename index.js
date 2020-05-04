66964dde9b7e409376431817d2f2dd773571b3cd
Aleksandr Saiyan <anverch@gmail.com>
2020-05-03 21:44:24 -0500

    add questions

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const questions = [
+const inquirer = require('inquirer');
 
+const questions = [{
+        type: "input",
+        message: "What is your Github user name?",
+        name: "username",
+        validate: value => isNotEmpty(value)
+    },
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title",
+        validate: value => isNotEmpty(value)
+    },
+    {
+        type: "input",
+        message: "What is the project description?",
+        name: "description",
+        validate: value => isNotEmpty(value)
+    }
 ];
 
-function writeToFile(fileName, data) {
+function isNotEmpty(value) {
+    if (value != null && value.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function init() {
+function writeToFile(fileName, data) {}
 
+async function init() {
+    const userInput = await inquirer.prompt(questions);
 }
 
 init();
