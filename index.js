9de890711b7376c33098171e4499871ae4a4cb05
Aleksandr Saiyan <anverch@gmail.com>
2020-05-06 22:15:52 -0500

    move imput fucntion to generateMarkdown.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs").promises;
 const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown")
 
 const questions = [{
         type: "input",
@@ -70,32 +71,7 @@ function isNotEmpty(value) {
 }
 
 const writeToFile = async (fileName, data) => {
-    const content = `# ${data.title.trim()}\n
-![GitHub](https://img.shields.io/github/license/${data.username.trim()}/${data.title.trim()})\n
-## Description\n\n${data.description.trim()}\n
-## Table of Contents\n
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
-* [Contributing](#contributing)
-* [Tests](#tests)
-* [Questions](#questions)\n
-## Installation\n
-To install necessary dependencies, run the following command:
-\`${data.installation.trim()}\`\n
-## Usage\n
-${data.usage.trim()}\n
-## License\n
-This project is licensed under the ${data.license.trim()} license\n
-## Contributing\n
-${data.contributing.trim()}\n
-## Tests\n
-To run test, run the following command:\n
-\`${data.test.trim()}\`\n
-## Questions\n
-<img src="https://avatars.githubusercontent.com/${data.username.trim()}" style="width: 40px; hight:40px; border-radius:100%;">\n
-If you have any questions about the repo, open an issue or contact ${data.email.trim()}.`;
-
+    const content = generateMarkdown(data);
     await fs.writeFile(fileName, content);
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,30 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
-
-`;
+  # ${data.title.trim()}\n
+  ![GitHub](https://img.shields.io/github/license/${data.username.trim()}/${data.title.trim()})\n
+  ## Description\n\n${data.description.trim()}\n
+  ## Table of Contents\n
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)\n
+  ## Installation\n
+  To install necessary dependencies, run the following command:
+  \`${data.installation.trim()}\`\n
+  ## Usage\n
+  ${data.usage.trim()}\n
+  ## License\n
+  This project is licensed under the ${data.license.trim()} license\n
+  ## Contributing\n
+  ${data.contributing.trim()}\n
+  ## Tests\n
+  To run test, run the following command:\n
+  \`${data.test.trim()}\`\n
+  ## Questions\n
+  <img src="https://avatars.githubusercontent.com/${data.username.trim()}" style="width: 40px; hight:40px; border-radius:100%;">\n
+  If you have any questions about the repo, open an issue or contact ${data.email.trim()}.`;
 }
 
 module.exports = generateMarkdown;
